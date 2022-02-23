dc7b4d9c7721c22348b553898c790bb7c1167d1c
VGLoic <vincent.genod.loic@gmail.com>
2022-02-23 12:32:09 +0100

    feat: add missing tests

--- src/__tests__/display-players.test.tsx
+++ src/__tests__/display-players.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within, act } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import { setupEthTesting } from "eth-testing";
 import { ethers } from "ethers";
 import AppProviders from "providers";
@@ -43,7 +43,7 @@ describe("Display player list", () => {
     testingUtils.clearAllMocks();
   });
 
-  test("the table should display the list of players", async () => {
+  test("the table should display the list of players and update with events", async () => {
     testingUtils.mockConnectedWallet(
       ["0xA6d6126Ad67F6A64112FD875523AC20794e805af"],
       { chainId: "0x5" }
@@ -98,15 +98,15 @@ describe("Display player list", () => {
 
     render(<Players />, { wrapper: TestProviders });
 
-    const table = await screen.findByRole("table", { name: /players table/i });
+    await screen.findByRole("table", { name: /players table/i });
 
-    await within(table).findByText(/0xA6d...5af/i);
-    await within(table).findByText(/rgb\(123, 23, 124\)/i);
-    await within(table).findByText(/1.0 ETH/i);
+    await screen.findByText(/0xA6d...5af/i);
+    await screen.findByText(/rgb\(123, 23, 124\)/i);
+    await screen.findByText(/1.0 ETH/i);
 
-    await within(table).findByText(/0x3E6...284/i);
-    await within(table).findByText(/rgb\(12, 232, 12\)/i);
-    await within(table).findByText(/1.1 ETH/i);
+    await screen.findByText(/0x3E6...284/i);
+    await screen.findByText(/rgb\(12, 232, 12\)/i);
+    await screen.findByText(/1.1 ETH/i);
 
     act(() => {
       rainbowTokenTestingUtils.mockEmitLog("PlayerJoined", [
@@ -115,12 +115,42 @@ describe("Display player list", () => {
       ]);
     });
 
-    await within(table).findByText(/0x39E...c7A/i, undefined, {
+    await screen.findByText(/0x39E...c7A/i, undefined, {
       timeout: 4500,
     });
-    await within(table).findByText(/rgb\(0, 0, 0\)/i, undefined, {
+    await screen.findByText(/rgb\(0, 0, 0\)/i, undefined, {
       timeout: 4500,
     });
-    await within(table).findByText(/0.1 ETH/i, undefined, { timeout: 4500 });
-  });
+    await screen.findByText(/0.1 ETH/i, undefined, { timeout: 4500 });
+
+    act(() => {
+      rainbowTokenTestingUtils.mockEmitLog("BlendingPriceUpdated", [
+        "0x39EB1f596CB19eE8c0DFef0391BE9B7201b2ac7A",
+        ethers.utils.parseUnits("2.1", "ether").toHexString(),
+      ]);
+    });
+    await screen.findByText(/2.1 ETH/i, undefined, { timeout: 4500 });
+
+    act(() => {
+      rainbowTokenTestingUtils.mockEmitLog("Blended", [
+        "0x39EB1f596CB19eE8c0DFef0391BE9B7201b2ac7A",
+        "0xA6d6126Ad67F6A64112FD875523AC20794e805af",
+        { r: 0, g: 127, b: 127 },
+        { r: 0, g: 255, b: 255 },
+      ]);
+    });
+    await screen.findByText(/rgb\(0, 127, 127\)/i, undefined, {
+      timeout: 4500,
+    });
+
+    act(() => {
+      rainbowTokenTestingUtils.mockEmitLog("SelfBlended", [
+        "0x39EB1f596CB19eE8c0DFef0391BE9B7201b2ac7A",
+        { r: 0, g: 63, b: 63 },
+      ]);
+    });
+    await screen.findByText(/rgb\(0, 63, 63\)/i, undefined, {
+      timeout: 4500,
+    });
+  }, 30000);
 });
